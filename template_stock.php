96744e75a98f18fec883673ad7ed9e72322e1887
vennro <vennro@gmail.com>
2026-05-22 14:28:51 +0700

    save

--- template_stock.php
+++ template_stock.php
@@ -11,20 +11,24 @@ jullybride_template_part('components/story-overlays');
 <main class="jb-main jb-editorial-page jb-stock jb-stock-archive jb-striped-arch">
     <?php jullybride_template_part('components/story-carousel', ['section_class' => 'jb-story-carousel-section']); ?>
 
-    <div class="container">
-        <?php
-        while (have_posts()) {
-            the_post();
-
+    <?php
+    while (have_posts()) {
+        the_post();
+        ?>
+        <div class="container">
+            <?php
             jullybride_breadcrumbs();
             jullybride_template_part('stock/header', [
                 'title' => get_the_title(),
             ]);
             jullybride_template_part('stock/hero-list');
             jullybride_template_part('stock/sale-products');
-        }
-        ?>
-    </div>
+            ?>
+        </div>
+        <?php
+        jullybride_template_part('stock/important-cta');
+    }
+    ?>
 </main>
 <?php
 get_footer();
